e2f0f7badd9719dcf542317290f399953d7a15bf
Kerry Gilbert <kerry@punkave.com>
2013-10-22 16:21:38 -0400

    readme v1 and source files

--- README.md
+++ README.md
@@ -2,3 +2,29 @@ smorgasbord
 ===========
 
 jQuery plugin to generate "sample player" UI for HTML5 Audio.
+
+###usage
+
+First, make sure you've added some audio files to your markup. They should look like this:
+
+```
+<audio src="sounds/one.mp3" preload="auto"></audio>
+<audio src="sounds/two.mp3" preload="auto"></audio>
+<audio src="sounds/three.m4a" preload="auto"></audio>
+<audio src="sounds/four.mp3" preload="auto"></audio>
+```
+
+Now, if you've included the smorgasbord plugin in your project, all you need to do is:
+
+```
+$('audio').bord();
+```
+###available options
+
+```
+$('audio').bord({
+  keyboard: true,
+  loopable: true
+});
+```
+
---
+++ smorgasbord.js
@@ -0,0 +1,69 @@
+$.fn.bord = function(userOptions) {
+  var audios = this;
+  var options = userOptions || {};
+
+  audios.each(function(index){
+    var that = $(this);
+    var sound = $(this).get(0);
+
+    if(!that.attr('id')) {
+      var croppedSrc = that.attr('src').split('.')[0].split('/');
+      croppedSrc = croppedSrc[croppedSrc.length-1];
+      that.attr('id', croppedSrc);
+    }
+
+    var id = that.attr('id');
+
+    that.removeAttr('controls');
+    var control = '<div class="smorgas btn s-'+(index+1)+'" id="'+id+'">'+id+'</div>';
+    $(control).appendTo(that.parent()).wrap('<div class="sound-container"></div>');
+    var btn = $('.btn#'+id);
+
+    if(options.loopable) {
+      btn.parent().append('<input class="toggle-loop" type="checkbox" name="loop">');
+      btn.parent().find('input').on('change', function(){
+        btn.toggleClass('loops');
+      });
+    }
+
+    btn.on('click', function(event){
+      if($(this).hasClass('playing')) {
+        stopSound($(this), sound);
+      } else {
+        playSound($(this), sound);
+      }
+    });
+
+    sound.addEventListener('canplaythrough', function(){
+      $('div#'+$(this).attr('id')).addClass('playable');
+    });
+
+    sound.addEventListener('ended', function(){
+      $('div#'+$(this).attr('id')).removeClass('playing');
+    });
+  });
+
+  function stopSound(btn, sound) {
+    sound.pause();
+    sound.currentTime = 0;
+    btn.removeClass('playing');
+  }
+
+  function playSound(btn, sound) {
+    $(sound).removeAttr('loop');
+    if(btn.hasClass('loops')) {
+      $(sound).attr('loop',true)
+    }
+    sound.play();
+    btn.addClass('playing');
+  }
+
+  if(options.keyboard) {
+    window.onkeydown = function(event){
+      var key = event.which;
+      if(key < 58 && key > 47) {
+        $('.btn.s-'+(key-48)).trigger('click');
+      } 
+    }
+  }
+}
